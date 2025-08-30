13e5a9ad85e6f5398703c7eb64b75a670be41d57
Md-Tahsif-Ahmed <tahsif.cse@gmail.com>
2025-08-30 18:23:46 +0600

    user aauth profiel

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { UserRoutes } from '../modules/user/user.routes';
 import { AuthRoutes } from '../modules/auth/auth.routes';
+import { ProfileRoutes } from '../modules/profile/profile.routes';
 const router = express.Router();
 
 const apiRoutes = [
     { path: "/user", route: UserRoutes },
     { path: "/auth", route: AuthRoutes },
+    { path: '/profile', route: ProfileRoutes }
 ]
 
 apiRoutes.forEach(route => router.use(route.path, route.route));
